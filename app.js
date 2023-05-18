5e642dc76c6ed4a3ae37c38bc8e677333ffb5c45
Clinton Harwood <harwood.clinton@gmail.com>
2023-05-18 16:25:36 +1000

    Initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var app = express();
+
+var apiV1 = require("./apiv1.js");
+var apiV2 = require("./apiv2.js");
+
+app.set("port", process.env.PORT || 3000);
+
+app.use("/v1", apiV1);
+app.use("/v2", apiV2);
+
+app.listen(3000, function() {
+    console.log("App started on port 3000");
+});
