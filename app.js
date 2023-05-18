14ddd157c731c4a38d54f4c5ebecf920a1a07fd0
Clinton Harwood <harwood.clinton@gmail.com>
2023-05-18 21:20:27 +1000

    Added logging with morgan

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 var express = require("express");
+var morgan = require("morgan");
 
 var app = express();
+
 
 var apiV1 = require("./apiv1.js");
 var apiV2 = require("./apiv2.js");
 
 app.set("port", process.env.PORT || 3000);
 
+app.use(morgan("short"));
 app.use("/v1", apiV1);
 app.use("/v2", apiV2);
 
