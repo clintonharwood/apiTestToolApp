1fccba1c2c0035d0b99c7bc4773e3cb7f4396b2a
Clinton Harwood <harwood.clinton@gmail.com>
2023-05-19 10:54:25 +1000

    removed API versioning

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 var express = require("express");
-var morgan = require("morgan");
 
 var app = express();
 
-
-var apiV1 = require("./apiv1.js");
-var apiV2 = require("./apiv2.js");
-
 app.set("port", process.env.PORT || 3000);
 
-app.use("/v1", apiV1);
-app.use("/v2", apiV2);
+app.get("/v1/all", function(req, res) {
+    res.send("You just sent a GET request to v1");
+});
+
+app.post("/v1", function(req, res) {
+    res.send("A POST request to v1");
+});
+
+app.put("/v1", function(req, res) {
+    res.send("A PUT request to v1");
+});
+
+app.delete("/v1", function(req, res) {
+    res.send("A DELETE request to v1");
+});
 
 app.listen(3000, function() {
     console.log("App started on port 3000");
