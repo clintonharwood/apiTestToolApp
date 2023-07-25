b0ff73374a82fd8d55948cacf691f4fdb3a577a0
Clinton Harwood <harwood.clinton@gmail.com>
2023-07-25 14:40:18 +1000

    Added an easter egg

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.get("/", function(req, res) {
     res.render("index");
 });
 
+app.get("/cmoney", function(req, res) {
+    res.render("cmoney");
+});
+
 app.get("/v1/all", function(req, res) {
     res.status(200);
     res.json({"products": [{ "id": 1, "title": "Phone", "description": "A mobile which is nothing like apple", "price": 549, "discountPercentage": 12.96, "rating": 4.69, "stock": 94, "category": "smartphones" }, { "id": 2, "title": "Wallet", "description": "Plain leather wallet", "price": 89, "discountPercentage": 17.94, "rating": 4.44, "stock": 34, "category": "wallets" }, { "id": 3, "title": "Hat", "description": "Plain black baseball hat", "price": 20, "discountPercentage": 15.46, "rating": 4.09, "stock": 36, "category": "clothing" }]});
