46d09a1e88c18885ee23de333859700f8c23a4d9
Clinton Harwood <harwood.clinton@gmail.com>
2024-05-27 13:33:39 +1000

    Modified iframe page

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(
 	helmet({
 	  contentSecurityPolicy: {
 		directives: {
-			frameSrc: ['https://https://devorg0923com-dev-ed--c.develop.vf.force.com']
+			frameSrc: ['https://devorg0923com-dev-ed--c.develop.vf.force.com']
 		},
 	  },
 	}),
