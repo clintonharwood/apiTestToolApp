fc7e9b932e78d29b647cbf2e0a60417250947dab
Clinton Harwood <harwood.clinton@gmail.com>
2025-04-22 10:33:12 +1000

    Prettyfy all view pages and api version files

--- apiv1.js
+++ apiv1.js
@@ -2,20 +2,20 @@ var express = require("express");
 
 var api = express.Router();
 
-api.get("/", function(req, res) {
-    res.send("You just sent a GET request to v1");
+api.get("/", function (req, res) {
+  res.send("You just sent a GET request to v1");
 });
 
-api.post("/", function(req, res) {
-    res.send("A POST request to v1");
+api.post("/", function (req, res) {
+  res.send("A POST request to v1");
 });
 
-api.put("/", function(req, res) {
-    res.send("A PUT request to v1");
+api.put("/", function (req, res) {
+  res.send("A PUT request to v1");
 });
 
-api.delete("/", function(req, res) {
-    res.send("A DELETE request to v1");
+api.delete("/", function (req, res) {
+  res.send("A DELETE request to v1");
 });
 
 module.exports = api;
--- apiv2.js
+++ apiv2.js
@@ -2,21 +2,21 @@ var express = require("express");
 
 var api = express.Router();
 
-api.get("/", function(req, res) {
-    res.status(200);
-    res.send("You just sent a GET request to v2");
+api.get("/", function (req, res) {
+  res.status(200);
+  res.send("You just sent a GET request to v2");
 });
 
-api.post("/", function(req, res) {
-    res.send("A POST request to v2");
+api.post("/", function (req, res) {
+  res.send("A POST request to v2");
 });
 
-api.put("/", function(req, res) {
-    res.send("A PUT request to v2");
+api.put("/", function (req, res) {
+  res.send("A PUT request to v2");
 });
 
-api.delete("/", function(req, res) {
-    res.send("A DELETE request to v2");
+api.delete("/", function (req, res) {
+  res.send("A DELETE request to v2");
 });
 
 module.exports = api;
