36f51430d17cc088b10a7291ac03aae5bc146a8e
Clinton Harwood <harwood.clinton@gmail.com>
2025-08-18 21:51:45 +1000

    Removed iframe cors

--- app.js
+++ app.js
@@ -55,7 +55,6 @@ app.use(
           "https://devorg0923com-dev-ed--c.develop.vf.force.com",
           "https://api.clintox.xyz",
           "https://devorg0923com-dev-ed.develop.lightning.force.com",
-          "https://truemoney.my.salesforce-sites.com"
         ],
         scriptSrc: [
           "'self'",
