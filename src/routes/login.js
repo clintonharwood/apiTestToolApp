dab6ddb1501c5e0d49d41fe4bc90d7e420b2d136
Clinton Harwood <harwood.clinton@gmail.com>
2026-06-10 15:47:37 +1000

    IdP init bug fix

--- src/routes/login.js
+++ src/routes/login.js
@@ -110,12 +110,14 @@ router.post("/login", (req, res, next) => {
   passport.authenticate("local", (err, user) => {
     if (err) return next(err);
     if (!user) return res.redirect("/login?error=1");
+    const pendingSamlRequest = req.session.pendingSamlRequest;
     req.session.regenerate((regenErr) => {
       if (regenErr) return next(regenErr);
       req.logIn(user, { session: false }, (loginErr) => {
         if (loginErr) return next(loginErr);
         req.session.passport = { user: user._id.toString() };
-        const redirectTo = req.session.pendingSamlRequest ? "/idp/sso" : "/";
+        if (pendingSamlRequest) req.session.pendingSamlRequest = pendingSamlRequest;
+        const redirectTo = pendingSamlRequest ? "/idp/sso" : "/";
         req.session.save((saveErr) => {
           if (saveErr) return next(saveErr);
           res.redirect(redirectTo);
