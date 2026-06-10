6b8309cb38e9e8044bbb27bb55ff584994a766ee
Clinton Harwood <harwood.clinton@gmail.com>
2026-06-10 20:15:02 +1000

    Add authnstatement to smal response

--- src/config/idpConfig.js
+++ src/config/idpConfig.js
@@ -59,6 +59,34 @@ if (idpConfigured) {
     ],
     isAssertionEncrypted: false,
     wantAuthnRequestsSigned: !!process.env.SP_CERT,
+    // samlify defaults AuthnStatement to '' in its tag-value map, so the
+    // placeholder is always dropped unless we bake it into the template.
+    loginResponseTemplate: {
+      context:
+        '<samlp:Response xmlns:samlp="urn:oasis:names:tc:SAML:2.0:protocol" xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" ID="{ID}" Version="2.0" IssueInstant="{IssueInstant}" Destination="{Destination}" InResponseTo="{InResponseTo}">' +
+        '<saml:Issuer>{Issuer}</saml:Issuer>' +
+        '<samlp:Status><samlp:StatusCode Value="{StatusCode}"/></samlp:Status>' +
+        '<saml:Assertion xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xs="http://www.w3.org/2001/XMLSchema" xmlns:saml="urn:oasis:names:tc:SAML:2.0:assertion" ID="{AssertionID}" Version="2.0" IssueInstant="{IssueInstant}">' +
+        '<saml:Issuer>{Issuer}</saml:Issuer>' +
+        '<saml:Subject>' +
+        '<saml:NameID Format="{NameIDFormat}">{NameID}</saml:NameID>' +
+        '<saml:SubjectConfirmation Method="urn:oasis:names:tc:SAML:2.0:cm:bearer">' +
+        '<saml:SubjectConfirmationData NotOnOrAfter="{SubjectConfirmationDataNotOnOrAfter}" Recipient="{SubjectRecipient}" InResponseTo="{InResponseTo}"/>' +
+        '</saml:SubjectConfirmation>' +
+        '</saml:Subject>' +
+        '<saml:Conditions NotBefore="{ConditionsNotBefore}" NotOnOrAfter="{ConditionsNotOnOrAfter}">' +
+        '<saml:AudienceRestriction><saml:Audience>{Audience}</saml:Audience></saml:AudienceRestriction>' +
+        '</saml:Conditions>' +
+        '<saml:AuthnStatement AuthnInstant="{IssueInstant}">' +
+        '<saml:AuthnContext>' +
+        '<saml:AuthnContextClassRef>urn:oasis:names:tc:SAML:2.0:ac:classes:Password</saml:AuthnContextClassRef>' +
+        '</saml:AuthnContext>' +
+        '</saml:AuthnStatement>' +
+        '{AttributeStatement}' +
+        '</saml:Assertion>' +
+        '</samlp:Response>',
+      attributes: [],
+    },
   });
 
   const spConfig = {
